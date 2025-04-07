d584af11f2893915fa8af2f89eee6a940a05b85d
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-04-07 00:10:09 -0300

    Organizando seção 02

--- Seção 02 - The GIST of LangChain/information.py
+++ Seção 02 - The GIST of LangChain/information.py
@@ -1,9 +1,4 @@
-from langchain_core.prompts import PromptTemplate
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-from pydantic import BaseModel, Field
-
-information = """
+INFORMATION = """
 Vladimir Vladimirovitch Putin, Leningrado, 7 de outubro de 1952) é um político
 russo, ex-advogado e ex-oficial de inteligência que atua como presidente da
 Rússia desde 2012. Putin ocupou cargos contínuos como presidente ou
@@ -30,34 +25,3 @@ protestos, sendo reeleito em 2018. Em abril de 2021, após um referendo, ele
 sancionou emendas constitucionais que lhe permitiam concorrer à reeleição mais
 duas vezes, potencialmente estendendo sua presidência até 2036.
 """
-
-if __name__ == "__main__":
-    load_dotenv()
-
-    print("Hello, LangChain! 🦜🔗")
-
-    class Output(BaseModel):
-        resume: str = Field(description="A resposta contendo o pequeno resumo.")
-        factories: list[str] = Field(
-            description="A lista contendo os fatos interessantes."
-        )
-
-    summary_prompt = """
-        Fornecida informações {information} sobre uma pessoa, quero que você crie:
-        1. um pequeno resumo
-        2. dois fatos interessantes sobre ela
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_prompt
-    )
-
-    llm = ChatGroq(temperature=0, model="llama3-70b-8192").with_structured_output(
-        Output
-    )
-
-    chain = summary_prompt_template | llm
-
-    res = chain.invoke(input={"information": information})
-
-    print(res)
