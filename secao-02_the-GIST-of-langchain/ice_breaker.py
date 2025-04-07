d86fb51b81743386d24917657355a19efbb39361
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-04-07 18:56:41 -0300

    Refactor ice_breaker.py for improved readability and consistency in string formatting

--- secao-02_the-GIST-of-langchain/ice_breaker.py
+++ secao-02_the-GIST-of-langchain/ice_breaker.py
@@ -1,40 +1,41 @@
+from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
-from dotenv import load_dotenv
+from loguru import logger
 from pydantic import BaseModel, Field
 
 from information import INFORMATION as information
 
-from loguru import logger
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     load_dotenv()
 
-    logger.info("Hello, LangChain! 🦜🔗")
+    logger.info('Hello, LangChain! 🦜🔗')
 
     class Output(BaseModel):
-        resume: str = Field(description="A resposta contendo o pequeno resumo.")
+        resume: str = Field(
+            description='A resposta contendo o pequeno resumo.'
+        )
         factories: list[str] = Field(
-            description="A lista contendo os fatos interessantes."
+            description='A lista contendo os fatos interessantes.'
         )
 
     summary_prompt = """
-        Fornecida informações {information} sobre uma pessoa, quero que você crie:
+        Fornecida informações {information} sobre uma pessoa,
+        quero que você crie:
         1. um pequeno resumo
         2. dois fatos interessantes sobre ela
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_prompt
+        input_variables=['information'], template=summary_prompt
     )
 
-    llm = ChatGroq(temperature=0, model="llama3-70b-8192").with_structured_output(
-        Output
-    )
+    llm = ChatGroq(
+        temperature=0, model='llama3-70b-8192'
+    ).with_structured_output(Output)
 
     chain = summary_prompt_template | llm
 
-    res = chain.invoke(input={"information": information})
+    res = chain.invoke(input={'information': information})
 
     print(res)
